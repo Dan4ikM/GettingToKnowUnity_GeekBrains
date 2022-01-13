f3c90da2b557417e5eccd333448e928e71dd763c
DaniilMoi <moiseev@179.ru>
2022-01-13 21:18:06 +0300

    Create task 3

---
+++ MyProject_GettingToKnowUnity_GB/Assets/Scripts/RocketView.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketView : MonoBehaviour
+{
+
+    RocketController _rocketControlle = new RocketController();
+
+    private void Start()
+    {
+            
+    }
+
+    private void NextLevel() => _rocketControlle.NextLevel();
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "FinishPad":
+                Invoke("NextLevel", 2f);
+                break;
+            default:
+                break;
+        }
+    }
+}
